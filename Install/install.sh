a2578c86bcfb31567bbf0b9fbfbfeeb74f0b6e13
SecureShelter <92108223+SecureShelter@users.noreply.github.com>
2021-10-07 19:14:33 +0200

    Add files via upload

---
+++ Install/install.sh
@@ -0,0 +1,11 @@
+ 
+#!/bin/sh
+sudo apt-get install git  python3 python3-pip nmap wkhtmltopdf python3-tk xsltproc python3-pyqt5
+pip3 install tld requests tabulate tld requests pandas colorama termcolor dnspython==2.0.0 
+pip3 install ip2geotools
+pip3 install validators
+pip3 install python-whois
+pip3 install folium
+pip3 install python3-nmap
+sudo apt-get install -y python3-pyqt5.qtwebkit.
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+![recoon-servers_p](https://user-images.githubusercontent.com/59021489/120799518-220fbb80-c53f-11eb-9306-5b616e898871.jpg)
+![Prst](https://user-images.githubusercontent.com/59021489/120805787-766a6980-c546-11eb-8e45-70cea8672d75.gif)
+![recoon-servers_p2](https://user-images.githubusercontent.com/59021489/120900050-261d0580-c633-11eb-9669-23c0e4622e90.jpg)
