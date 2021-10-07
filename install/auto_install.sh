358cf4d708080cb3a0cc29647e0e646f3a514699
SecureShelter <92108223+SecureShelter@users.noreply.github.com>
2021-10-07 19:13:05 +0200

    Delete install directory

--- install/auto_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-sudo apt-get install git  python3 python3-pip nmap nikto wkhtmltopdf python3-tk xsltproc python3-pyqt5
-
-sudo apt-get install -y python3-pyqt5.qtwebkit.
-pip3 install python3-nmap
-python3 Install_Hyperionv3.py
--- install/kali.md
+++
@@ -1,3 +0,0 @@
-pip3 install --user --upgrade PyQt5
-pip3 install --user --upgrade PyQtWebEngine
-sudo apt-get install libxcb-render-util0 libxcb-image0 libxcb-keysyms1 libxcb-icccm4
